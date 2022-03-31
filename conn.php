e9e2e8501068f93c2afc1136a04cbf152ff40e40
Muhammad Miftachudin <muhammad.miftachudin55@gmail.com>
2022-03-31 11:57:42 +0700

    ganti host

--- conn.php
+++ conn.php
@@ -8,7 +8,7 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$host = 'http://localhost:7000/owaspphp/';
+$host = 'https://w2.swift.id/';
 
 error_reporting(E_ALL);
 
