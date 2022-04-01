8db4f9d6379932a39b062f212f49a3725c07efa5
Muhammad Miftachudin <muhammad.miftachudin55@gmail.com>
2022-04-01 09:45:08 +0700

    add recaptcha v3

--- signin.php
+++ signin.php
@@ -1,6 +1,7 @@
 <?php
 include "header.php";
 ?>
+<script src="https://www.google.com/recaptcha/api.js"></script>
 
 <div class="login-clean">
         <form method="post" action="<?php echo $host;?>function/actSignin.php">
@@ -18,8 +19,15 @@ include "header.php";
             <div class="illustration"><i class="fa fa-ticket"></i></div>
             <div class="form-group"><input class="form-control" type="email" required="required" name="email" placeholder="Email"></div>
             <div class="form-group"><input class="form-control" type="password" required="required" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Sign In</button></div><a class="forgot" href="forgotPassword.php">Forgot your email or password?</a></form>
+            <div class="form-group"><button class="btn btn-primary btn-block g-recaptcha" data-sitekey="6LeY5DQfAAAAABxf9eSPGvPzYKylL_S68sLvtvmb" data-callback='onSubmit' data-action='submit'type="submit">Sign In</button>
+            </div><a class="forgot" href="forgotPassword.php">Forgot your email or password?</a></form>
 </div>
+
+<script>
+   function onSubmit(token) {
+     document.getElementById("demo-form").submit();
+   }
+ </script>
 
 <?php
 include "footer.php";
